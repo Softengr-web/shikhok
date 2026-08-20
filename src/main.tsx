446bc82514689eb876c77732baff55aefbe8873e
Softengr-web <tanviralamprince0@gmail.com>
2026-08-20 21:20:22 +0600

    Show Gig storefront on teacher profiles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import './styles.css';
 import './layout-overrides.css';
 import './gig-builder.css';
 import './gig-capabilities.css';
+import './teacher-profile-gigs.css';
 import { App } from './App';
 
 createRoot(document.getElementById('root')!).render(<StrictMode><App /></StrictMode>);
